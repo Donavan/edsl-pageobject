fa94d7da6fd5d86b4d836048c891eb5c092fd60e
Donavan Stanley <donavan.stanley@gmail.com>
2018-10-21 20:45:30 -0400

    Break out page object pattern stuff from edsl

---
+++ lib/edsl/page_object.rb
@@ -0,0 +1,6 @@
+require 'edsl'
+require_relative 'page_object/population'
+require_relative 'page_object/page'
+require_relative 'page_object/section'
+require_relative 'page_object/navigation'
+
---
+++ lib/edsl/page_object/version.rb
@@ -0,0 +1,5 @@
+module EDSL
+  module PageObject
+    VERSION = '0.1.0'.freeze
+  end
+end
